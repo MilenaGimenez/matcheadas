62aefa63d792bbae5c5867417e42ab9ceedec500
MilenaGimenez <milenanicolegimenez@gmail.com>
2021-03-07 19:48:16 -0300

    UPDATE/Añadiendo timer y modal de juego finalizado

--- js/app.js
+++ js/app.js
@@ -1,51 +1,48 @@
-/* const root = document.getElementById('restart-box')
-const restartButton = document.getElementById('restart-button');
-
-restartButton.addEventListener('click', (e) => {
-  
-  swal("¡Perderás todo tu puntaje acumulado!", {
-    title: "¿Reiniciar juego?",
-    buttons: ["Cancelar", "Nuevo juego"],
-  }); 
-  
-})
- */
-
-
 //GRILLA
 
 const tab = document.getElementById('grid-box');
 
-let items = ["🌎", "⭐", "🌙", "🚀", "🌀"];
+let items = ["🌎", "⭐", "🌙", "🚀", "🪐"];
+const helpButton = document.getElementById('help-button');
+const restartButton = document.getElementById('restart-button');
 
-/* let dificulty = 0; */
 
-const facil = 9;
-const normal = 8;
-const dificil = 7;
 
-let level;
+const timer = document.getElementById('time-number')
+let time;
+let restTime;
+
+
+let optionWelcome = true;
+
+
+let level = 0;
+
 
 const tamanoGrilla = (nivel, div) => {
-    /* div.style.width = `calc(32rem / ${nivel}) - 1.02rem`
-    div.style.height = `calc(32rem / ${nivel}) - 1.02rem` */
-    /* div.style.width = `calc(500px / ${nivel}) - 1.02rem`
-    div.style.height = `calc(500px / ${nivel}) - 1.02rem` */
-    div.style.width = `${500 / level}px`;
+    div.style.width = `calc(32rem / ${nivel}) - 1.02rem`
+    div.style.height = `calc(32rem / ${nivel}) - 1.02rem` 
+}
 
-
-const crearTablero = (nivel) => {
-    for (let i = 0; i < nivel; i++) {        
-        for (let j = 0; j < nivel; j++) {
-            const newd = document.createElement("div");
-            /* tamanoGrilla(nivel, newd) */
-            newd.innerHTML = items[getRandom(0, 4)];
-            tab.appendChild(newd);
-            newd.style.width = `${500/nivel}px`
-            newd.style.height = `${500/nivel}px`
-            /* newd.style.fontSize = `${40+nivel-6}px` */
-        }
+const crearTablero = () => {
+    time = 30;
+    timer.innerHTML = `00:${time}`
+    while (tab.firstChild) {
+        tab.removeChild(tab.firstChild);
     }
+    for (let i = 0; i < level; i++) {        
+        for (let j = 0; j < level; j++) {
+            const newd = document.createElement("div");
+            tamanoGrilla(level, newd)
+            newd.innerHTML = items[getRandom(0, 5)];
+            tab.appendChild(newd);
+            newd.style.width = `${500/level}px`
+            newd.style.height = `${500/level}px`
+            twemoji.parse(document.body);
+        }
+    };
+    restTime = setInterval(myTimer, 1000);
+    return level
 };
 
 const getRandom = (min, max) => {
@@ -53,31 +50,33 @@ const getRandom = (min, max) => {
     return Math.floor(random * (max - min)) + min;
 }
 
-/* const obtenerEmoj = console.log(`posicion:`, items[getRandom(0, 4)]) */
+
+window.onload = welcomeModal();
 
 
-//getRandom(6, 10);
 
-crearTablero();
+helpButton.addEventListener('click', welcomeModal)
+restartButton.addEventListener('click', restartGame)
 
 
-//Timer no funciona
-/* const contador = document.getElementById('time-number')
 
-let time = 30
+// Timer
 
-setInterval(()=>{
-    if(time > -1){
-        contador.innerHTML = time
-        time--
-    }else{
-        alert('juego terminado')
-        //swal juego terminado
-        return
-    }
-    function myStopFunction(){
-        clearInterval(time)
-    }
-}, 1000)
-
-myStopFunction() */
+const myTimer = () =>{
+    if(time <= 30 && time >0){
+      time =  time -1
+      let seconds = time % 60;
+      let minutes = ((time - seconds) / 60) % 60; 
+      let txtSeconds = seconds < 10 ? '0' + seconds : seconds
+      let txtMinutes = minutes < 10 ? '0' + minutes : minutes
+      timer.innerHTML = `${txtMinutes}:${txtSeconds}`
+  
+    } else {
+        finishGame()
+      }
+  };
+  
+  
+  function stopTimer() {
+    clearInterval(restTime);
+  }
